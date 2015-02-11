0e98f30f1195b61847bdd864ccda6eb5739b84c7
noams <noamshaish@gmail.com>
2015-02-11 10:18:18 +0200

    Code cleanup

--- src/main/scala/reporting/chatDuration.scala
+++ src/main/scala/reporting/chatDuration.scala
@@ -1,35 +1,22 @@
 package reporting
 
+import _root_.utils.QueueStream
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import reporting.utils.{RepEventUtil}
 
 /**
  * Created by noams on 1/29/15.
  */
-object chatDuration {
-  def getField(eventLine: String, index: Int) = eventLine.split(" ")(index)
+object ChatDuration {
 
-  def getEventType(eventLine: String) = getField(eventLine, 2)
+  def calculateDuration(eventLine: String) = {
+    val prevEventTime: Long = Math.max(RepEventUtil.getPrevRepSessionEventTime(eventLine).toLong, RepEventUtil.getPrevRepActivityEventTime(eventLine).toLong)
+    if (prevEventTime < 0) 0l else RepEventUtil.getEventTime(eventLine).toLong - prevEventTime
+  }
 
-  def isChatStatusChanged(eventLine: String): Boolean = List("1", "3", "4").contains(getType(eventLine))
-
-  def getType(eventLine: String): String = getField(eventLine, 6)
-
-  def isConnectedChatEvent(eventLine: String): Boolean = isRepEvent(eventLine) && isChatStatusChanged(eventLine)
-
-  def isRepEvent(eventLine: String): Boolean = getEventType(eventLine) == "RepSessionEvent" || getEventType(eventLine) == "RepActivityEvent"
-
-  def getSubType(eventLine: String) = getField(eventLine, 7)
-
-  def getPrevSubType(eventLine: String) = getField(eventLine, 11)
-
-  def getConcurrentEng(eventLine: String) = getField(eventLine, 19)
-
-  def getPrevConcurentEng(eventLine: String) = getField(eventLine, 18)
-
-  def getEventTime(eventLine: String) = getField(eventLine, 8)
-
-  def calculateDuration(eventLine: String) = getEventTime(eventLine)
+  def parseEvent(eventLine: String) = (RepEventUtil.getEventType(eventLine), calculateDuration(eventLine), "0", RepEventUtil.getSubType(eventLine),
+      RepEventUtil.getPrevSubType(eventLine), RepEventUtil.getConcurrentEng(eventLine), RepEventUtil.getPrevConcurentEng(eventLine))
 
   /**
    * Main entry to application
@@ -37,16 +24,17 @@ object chatDuration {
    */
   final def main (args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setAppName("Chat Duration")
-    sparkConf.setMaster("local[*]")
+    sparkConf.setMaster("local[10]")
       .set("spark.cassandra.connection.host", "127.0.0.1")
     val context = new StreamingContext(sparkConf, Seconds(1))
-    context.textFileStream(getClass.getResource("../reportEvents.txt").getPath)
-      .filter(row => isConnectedChatEvent(row))
-      .map(eventLine => (getEventType(eventLine), calculateDuration(eventLine), "0", getSubType(eventLine),
-      getPrevSubType(eventLine), getConcurrentEng(eventLine), getPrevConcurentEng(eventLine))).print()
+
+    QueueStream.getStream(context)
+      .filter(row => RepEventUtil.isConnectedChatEvent(row))
+      .map(parseEvent).print()
 
 
     context.start()
+    QueueStream.pushData(context, "../reportEvents.txt")
     context.awaitTermination()
   }
 }
--- src/main/scala/sentiment/sentimentsStream.scala
+++ src/main/scala/sentiment/sentimentsStream.scala
@@ -1,51 +1,36 @@
 package sentiment
 
+import _root_.utils.QueueStream
 import com.datastax.spark.connector.streaming._
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.streaming.dstream._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
  * Created by noams on 12/22/14.
  */
-abstract class sentimentsStream {
+object SentimentsStream {
 
-  /**
-   * @return name of Spark application
-   */
-  def getAppName(): String
-
-  /**
-   * @param context - Spark context
-   * @return input stream
-   */
-  def getStream(context: StreamingContext): DStream[String]
-
-  /**
-   * Push data to be consumed by Spark
-   * @param context - Spark context
-   */
-  def pushData(context: StreamingContext): Unit
 
   /**
    * Main entry to application
    * @param args - application arguments
    */
   final def main (args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setAppName(getAppName())
+    val sparkConf = new SparkConf().setAppName("Sentiment example")
     sparkConf.setMaster("local[*]")
     sparkConf.set("spark.cassandra.connection.host", "127.0.0.1")
 
     // Create the context
     val context = new StreamingContext(sparkConf, Seconds(1))
+    context.checkpoint("..")
 
-    new StreamComputation(context).compute(getStream(context))
+    new StreamComputation(context).compute(QueueStream.getStream(context))
       .reduceByKeyAndWindow((x: Int,y: Int) => x + y, (x: Int,y: Int) => x - y, Seconds(5), Seconds(2))
       .saveToCassandra("sparkstreaming", "sentimentCount")
 
     context.start()
-    pushData(context)
+    QueueStream.pushData(context, "../allPlines.txt")
     context.awaitTermination()
 
   }
